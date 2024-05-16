7987aae6e2ecbe3e0035bc3da27f29ed3e4ae77a
amirreza abbaszadeh <amirreza.a560@gmail.com>
2024-05-17 00:01:44 +0330

    fix csv, add defs to config and write main

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 from configparser import ConfigParser
-
+import psycopg2
 def load_config(filename='database.ini', section='postgresql'):
     parser = ConfigParser()
     parser.read(filename)
@@ -15,6 +15,36 @@ def load_config(filename='database.ini', section='postgresql'):
 
     return config
 
+
+def check_id(id):
+    config = load_config()
+    conn = psycopg2.connect(**config)
+    cur = conn.cursor()
+    sql = f"""
+    SELECT * FROM person WHERE id = {id}
+    """
+    cur.execute(sql)
+    result = cur.fetchall()
+    if len(result)> 0:
+        return True
+    else:
+        return False
+
+def check_tagname(name):
+    config = load_config()
+    conn = psycopg2.connect(**config)
+    cur = conn.cursor()
+    sql = f"""
+    SELECT tag_name FROM tags WHERE tag_name = {name};
+    """
+    cur.execute(sql)
+    result = cur.fetchall()
+    if len(result)> 0:
+        return True
+    else:
+        return False
+"""
 if __name__ == '__main__':
     config = load_config()
     print(config)
+    """
--- main.py
+++ main.py
@@ -1,9 +1,80 @@
 import psycopg2 as ps
-from config import load_config
+from config import load_config, check_id, check_tagname
+import csv
 #---------------------
 config = load_config()
 #---------------------
+image_result = []
+imagefilepath = "image.csv"
+with open (imagefilepath, newline='') as csvfile:
+    csv_reader = csv.reader(csvfile)
+    i = 0
+    for row in csv_reader:
+        if i == 0:
+            pass
+        else:
+            image_result.append(row)
+        i+=1
 
+conn = ps.connect(**config)
+cur = conn.cursor()
+for i in image_result:
+    if check_id(i[5]) == False:
+
+        sql1 = """
+            INSERT INTO person VALUES
+            (%s,%s,%s,%s)
+            """
+        data1 = (i[5],i[6],i[7],i[8])
+
+        sql2 = """
+            INSERT INTO images(path,title,description,category,p_id) VALUES
+            (%s,%s,%s,%s,%s)
+            """
+        data2 = (i[0], i[1], i[3], i[4], i[5])
+        try:
+            cur.execute(sql1,data1)
+        
+        except Exception as e:
+            print(e)
+        else:
+            conn.commit()
+            try:
+                cur.execute(sql2, data2)
+            except Exception as e:
+                print(e)
+            else:
+                conn.commit()
+            print("data imported.")
+    else:
+        sql1 = """
+            INSERT INTO images(path,title,description,category,p_id) VALUES
+            (%s,%s,%s,%s,%s)
+            """
+        data1 = (i[0], i[1], i[3], i[4], i[5])
+        try:
+            cur.execute(sql1,data1)
+        except Exception as e:
+            print(e)
+        else:
+            conn.commit()
+        sql2 = """
+            INSERT INTO tags(tag_name) VALUES
+            (%s)
+            """
+        tags = i[2].split(",")
+        for i in tags:
+            if check_tagname(i) == False:
+                try:
+
+                    cur.execute(sql2)
+                except Exception as e:
+                    print(e)
+                else:
+                    conn.commit()
+
+
+    
 
 
 
